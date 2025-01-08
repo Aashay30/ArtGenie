c26784cea8a9041556e4b9f957768b1388d8f88d
Aashay30 <tamrakaraashay30@gmail.com>
2025-01-08 18:11:38 +0530

    user routes added

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv/config";
 
 import connectdb from "./config/mongodb.js";
-// import userRouter from "./routes/userRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 // import imageRouter from "../server/routes/imageRoutes.js";
 
 const PORT = process.env.PORT || 4000;
@@ -14,7 +14,7 @@ app.use(cors());
 
 await connectdb();
 
-// app.use("/api/user", userRouter);
+app.use("/api/user", userRouter);
 // app.use("/api/image", imageRouter);
 
 app.get("/", (req, res) => res.send("API Working"));
