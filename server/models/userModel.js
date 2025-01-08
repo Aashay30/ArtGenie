37d3b2a9aa5fb0816709770f769f5d87507bfb32
Aashay30 <tamrakaraashay30@gmail.com>
2025-01-08 17:35:52 +0530

    user model and controller added

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name:{type:String, required:true},
-    email:{type:String, required:true, unique:true},
-    password:{type:String,required:true},
-    creditBalance: {type:Number, default:5},
-})
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  creditBalance: { type: Number, default: 20 },
+});
 
 const userModel = mongoose.model.user || mongoose.model("user", userSchema);
 
